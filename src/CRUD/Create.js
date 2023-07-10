bebf385e71a8385143b6d6a2792223d350bb0085
Tuong <aumanhtuong09122001@gmail.com>
2023-07-10 16:29:09 +0700

    hoan thanh part 2

--- src/CRUD/Create.js
+++ src/CRUD/Create.js
@@ -11,7 +11,9 @@ export default function CreateProduct() {
         name: "",
         price: '',
         number: '',
-        category: 0
+        category: {
+            id: ''
+        }
     });
     useEffect(() => {
         axios.get('http://localhost:8080/cate').then((response) => {
--- src/CRUD/Edit.js
+++ src/CRUD/Edit.js
@@ -1,4 +1,4 @@
-import {useParams, useNavigate} from "react-router-dom";
+import {useParams, useNavigate, Link} from "react-router-dom";
 import {Field, Form, Formik} from "formik";
 import axios from "axios";
 import {useEffect, useState} from "react";
@@ -6,29 +6,49 @@ import {useEffect, useState} from "react";
 export default function EditProduct() {
     const {id} = useParams();
     const navigate = useNavigate();
+    const [cate, setCate] = useState([])
+    // const [category,setCategory] = useState(null);
+    console.log('cate>>>>>>>', cate)
+
     const [product, setProduct] = useState({
         name: "",
         price: '',
-        number: ''
+        number: '',
+        category:{
+            id:''
+        }
     });
 
     useEffect(() => {
-        axios.patch(`http://localhost:8080/product/${id}`).then((response) => {
+        axios.patch(`http://localhost:8080/product/` + id).then((response) => {
             setProduct(response.data);
             console.log(product)
         });
     }, []);
 
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/cate').then((response) => {
+            setCate(response.data)
+        })
+    }, [])
+
+    const handleFormSubmit =
+        (values) => {
+            console.log('>>>>>>>>>>>>',values)
+            // setProduct({...product,category})
+            axios.put(`http://localhost:8080/product/` + id, values)
+                .then(() => {
+                    navigate("/");
+                })
+        }
+
+
     return (
         <>
             <h1>Edit Product</h1>
             <Formik initialValues={product}
-                    onSubmit={(values) => {
-                        axios.put(`http://localhost:8080/product/` + id, values)
-                            .then(() => {
-                                navigate("/");
-                            })
-                    }}
+                    onSubmit={handleFormSubmit}
                     enableReinitialize={true}>
                 <Form>
                     <table>
@@ -54,7 +74,12 @@ export default function EditProduct() {
                         <tr>
                             <td>Category</td>
                             <td>
-                                <Field type="text" name="number"/>
+                                <Field as="select" name="category.id" >
+                                    <option> Select Category</option>
+                                    {cate.map((item) => (
+                                        <option key={item.id} value={item.id}>{item.name}</option>
+                                    ))}
+                                </Field>
                             </td>
                         </tr>
 
@@ -63,6 +88,9 @@ export default function EditProduct() {
                         <tr>
                             <td>
                                 <button>Save</button>
+                                <Link to={"/"}>
+                                    <button>Back Home</button>
+                                </Link>
                             </td>
                         </tr>
                         </tbody>
